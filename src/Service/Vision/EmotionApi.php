410b450322007d5156837b7da519a71411a1e2cd
dakku <dakku@97634.no-reply.drupal.org>
2017-09-14 00:23:01 +0100

    POC of emotions API working yay.

--- src/Service/Vision/EmotionApi.php
+++ src/Service/Vision/EmotionApi.php
@@ -2,9 +2,28 @@
 
 namespace Drupal\azure_cognitive_services_api\Service\Vision;
 
+use Drupal\azure_cognitive_services_api\Service\Client;
+use Drupal\Core\Config\ConfigFactory;
+
 class EmotionApi {
+
+  const API_URL = '/emotion/v1.0/';
+
+  /**
+   * Constructor for the Emotion API class.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    $this->client = new Client($config_factory, 'emotion');
+  }
+
   // See https://westus.dev.cognitive.microsoft.com/docs/services/5639d931ca73072154c1ce89/operations/56f23eb019845524ec61c4d7
-  public function recognize() {}
+
+  public function recognize($photoUrl) {
+    $uri = self::API_URL . 'recognize';
+    $result = $this->client->doRequest($uri, 'POST', ['url' => $photoUrl]);
+    return $result;
+  }
+
   public function recognizeFaceRectangles() {}
   public function recognizeInVideo() {}
 
