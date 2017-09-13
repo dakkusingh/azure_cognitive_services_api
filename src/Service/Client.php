82539735a2d02153a27c718e1ba9e405fe2ff666
dakku <dakku@97634.no-reply.drupal.org>
2017-09-13 22:01:42 +0100

    adding client and revising namespace

---
+++ src/Service/Client.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\azure_cognitive_services_api\Service;
+
+use Drupal\Core\Config\ConfigFactory;
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client {
+
+  const API_BASE_URL = 'api.cognitive.microsoft.com';
+
+  /**
+   * Create the Azure Cognitive Services client.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    // Get the config.
+    $config = $config_factory->get('azure_cognitive_services_api.settings');
+    $this->subscriptionKey = $config->get('subscription_key');
+    $this->azureApiUri = 'https://' . $config->get('azure_region') . self::API_BASE_URL;
+
+    $this->guzzleClient = new GuzzleClient(
+      [
+        'base_uri' => $this->azureApiUri
+      ]
+    );
+  }
+
+  public function doRequest($uri, $method = 'GET', $body = []) {
+    try {
+      $response = $this->guzzleClient->request(
+        $method,
+        $this->azureApiUri . $uri,
+        [
+          'headers' => [
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => $this->subscriptionKey
+          ],
+          'json' => $body
+        ]
+      );
+    }
+    catch (\Exception $e) {
+      throw $e;
+    }
+
+    // TODO Check response.
+
+    return json_decode($response->getBody(), true);
+  }
+
+}
