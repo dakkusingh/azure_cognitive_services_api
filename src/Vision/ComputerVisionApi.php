8abcfed63d4b9a17bc4da3edb454226656c978db
dakku <dakku@97634.no-reply.drupal.org>
2017-09-13 16:31:08 +0100

    Adding computer vision api skeleton.

---
+++ src/Vision/ComputerVisionApi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\azure_cognitive_services_api\Vision;
+
+class ComputerVisionApi {
+  // See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1ff
+  public function analyze() {}
+  public function describe() {}
+  public function generateThumbnail() {}
+  public function ocr() {}
+  public function models() {}
+  public function recognizeText() {}
+  public function tag() {}
+
+}
