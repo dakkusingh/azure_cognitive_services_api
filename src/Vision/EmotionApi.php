9de25a297e3f4375f0482245166954d5a89af6aa
dakku <dakku@97634.no-reply.drupal.org>
2017-09-13 16:31:45 +0100

    Adding emotion api skeleton

---
+++ src/Vision/EmotionApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\azure_cognitive_services_api\Vision;
+
+class EmotionApi {
+  // See https://westus.dev.cognitive.microsoft.com/docs/services/5639d931ca73072154c1ce89/operations/56f23eb019845524ec61c4d7
+  public function recognize() {}
+  public function recognizeFaceRectangles() {}
+  public function recognizeInVideo() {}
+
+}
